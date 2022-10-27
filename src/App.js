71b6fe55b82abe9c6c40084959280cecd4351a98
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-27 11:17:09 +0300

    add contents to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import NavBar from './student-page/components/Navbar'
+import Sidebar from './student-page/components/Sidebar';
+import Resources from './student-page/components/Resources';
+import CalenDar from './student-page/components/Calendar';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+// import Tutorials from './student-page/components/Tutorials';
+// import Learn from './student-page/components/Learn';
+// import Events from './student-page/components/Events';
+// import Discussions from './student-page/components/Discussions';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <NavBar/> 
+
+          <div className='side-res'>
+            <Sidebar />
+            {/* <Tutorials /> */}
+            <Resources />    
+            <CalenDar/>  
+          </div>
+          {/* <Routes>
+            <Route exact path='/' element={<Tutorials/>}>
+            {/* <Tutorials/> */}
+            {/* </Route> 
+            <Route  path='learn'>
+            <Learn/>
+            </Route> 
+            <Route  path='/events'>
+            <Events/>
+            </Route> 
+          </Routes>
+         
+         */}
+      </div>
+     </Router>
   );
 }
 
