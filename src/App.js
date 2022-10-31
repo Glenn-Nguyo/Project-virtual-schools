9fb04ad65fec3e542db4052d4fad335a6dd756d1
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-31 15:00:25 +0300

    delete unnecessary imports

--- src/App.js
+++ src/App.js
@@ -4,39 +4,34 @@ import NavBar from './student-page/components/Navbar'
 import Sidebar from './student-page/components/Sidebar';
 import Resources from './student-page/components/Resources';
 import CalenDar from './student-page/components/Calendar';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-// import Tutorials from './student-page/components/Tutorials';
-// import Learn from './student-page/components/Learn';
-// import Events from './student-page/components/Events';
-// import Discussions from './student-page/components/Discussions';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-          <NavBar/> 
-
-          <div className='side-res'>
-            <Sidebar />
-            {/* <Tutorials /> */}
-            <Resources />    
-            <CalenDar/>  
-          </div>
-          {/* <Routes>
-            <Route exact path='/' element={<Tutorials/>}>
-            {/* <Tutorials/> */}
-            {/* </Route> 
-            <Route  path='learn'>
-            <Learn/>
-            </Route> 
-            <Route  path='/events'>
-            <Events/>
-            </Route> 
-          </Routes>
-         
-         */}
+    <BrowserRouter>
+      <NavBar />
+      <div className='side-res'>
+      <Sidebar />
+      <Resources />
+      <CalenDar />
       </div>
-     </Router>
-  );
-}
+     
+        <Routes>
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/calendar" element={<CalenDar />} />
 
+        </Routes>
+    </BrowserRouter>
+  
+  )
+}
 export default App;
+
+
+
+
+
+
+
+
+
